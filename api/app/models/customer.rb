d8675987b7a0f64c352b714782c7d3cb33d2e34a
George L. 'Maverick' Maluf <georgelmaluf286@gmail.com>
2021-12-22 21:25:08 -0300

    Add CPF validation on model

--- api/app/models/customer.rb
+++ api/app/models/customer.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
 class Customer < ApplicationRecord
+  validate :cpf_valid, on: %i[create update]
   validates :cpf, presence: true, uniqueness: true
+
+  def cpf_valid
+    return errors.add(:base, "CPF #{self.cpf} invalid") unless CPF.valid?(self.cpf)
+  end
+
+  def record_exists?
+    return false unless Customer.find_by_cpf(self.cpf).present?
+
+    true
+  end
 end
