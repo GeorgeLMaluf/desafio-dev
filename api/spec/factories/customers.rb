8c9740d176a8ea3713a66123a9b648db00a0c8ab
George L. 'Maverick' Maluf <georgelmaluf286@gmail.com>
2021-12-23 00:31:18 -0300

    Add FactoryBot for Customer model

---
+++ api/spec/factories/customers.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :customer do
+    cpf { Faker::CPF.numeric.to_s }
+
+    trait :invalid do
+      cpf { '123456' }
+    end
+  end
+end
