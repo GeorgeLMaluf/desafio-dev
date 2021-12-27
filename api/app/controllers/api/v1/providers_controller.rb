a61af4630c72a9ca3f4b0dcafd4ae81ea4a801df
George L. 'Maverick' Maluf <georgelmaluf286@gmail.com>
2021-12-27 20:40:00 -0300

    Add providers controller

---
+++ api/app/controllers/api/v1/providers_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class ProvidersController < Api::BaseApiController
+      def index
+        response = ::Providers::GetProviders.call
+
+        render json: response, status: :ok
+      end
+    end
+  end
+end
