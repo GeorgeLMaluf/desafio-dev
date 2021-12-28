d6d23a172a4396e10ed52c704b290ab2178dbaf4
George L. 'Maverick' Maluf <georgelmaluf286@gmail.com>
2021-12-28 18:08:39 -0300

    Add GetTransactionByProvider method and rspec related test

--- api/app/controllers/api/v1/transactions_controller.rb
+++ api/app/controllers/api/v1/transactions_controller.rb
@@ -3,9 +3,22 @@
 module Api
   module V1
     class TransactionsController < Api::BaseApiController
+      before_action :valid_params, only: [:by_provider]
+
       def index
         response = ::Transactions::GetTransactions.call
         render json: response.to_json, status: :ok
+      end
+
+      def by_provider
+        response = ::Transactions::GetTransactionsByProvider.call params['providerId']
+        render json: response.to_json, status: :ok
+      end
+
+      private
+
+      def valid_params
+        params.permit(:providerId)
       end
     end
   end
--- api/spec/requests/api/v1/transactions_spec.rb
+++ api/spec/requests/api/v1/transactions_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Transactions', type: :request do
-  describe 'GET /index' do
+  it 'GET /index' do
     headers = { CONTENT_TYPE: 'application/json' }
     get '/api/v1/transactions', headers: headers
+    expect(response.content_type).to eq('application/json; charset=utf-8')
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'GET /by_provider/:providerId' do
+    headers = { CONTENT_TYPE: 'application/json' }
+    providerId = 1
+    get "/api/v1/transactions/by_provider/#{providerId}", headers: headers
     expect(response.content_type).to eq('application/json; charset=utf-8')
     expect(response).to have_http_status(:ok)
   end
