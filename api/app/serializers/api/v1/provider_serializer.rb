fd756b52b3ffb86767c7278ae14411cd91d5d852
George L. 'Maverick' Maluf <georgelmaluf286@gmail.com>
2021-12-28 15:20:07 -0300

    Name capitalize when serialize a provider

--- api/app/serializers/api/v1/provider_serializer.rb
+++ api/app/serializers/api/v1/provider_serializer.rb
@@ -5,6 +5,14 @@ module Api
     class ProviderSerializer < Api::BaseSerializer
       attributes :name_provider,
                  :name_owner
+
+      def name_provider
+        object.name_provider.split(' ')&.map { |x| x.capitalize }.join(' ')
+      end
+
+      def name_owner
+        object.name_owner.split(' ')&.map { |x| x.capitalize }.join(' ')
+      end
     end
   end
 end
