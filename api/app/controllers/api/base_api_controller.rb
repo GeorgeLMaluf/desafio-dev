0d5a423b166829017eca0564fb5be9192e66b704
George L. 'Maverick' Maluf <georgelmaluf286@gmail.com>
2021-12-28 13:15:09 -0300

    Adjust base api controller

--- api/app/controllers/api/base_api_controller.rb
+++ api/app/controllers/api/base_api_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 module Api
-  class BaseApiController < ActionController::Base
-    # respond_to :json
+  class BaseApiController < ApplicationController
   end
 end
