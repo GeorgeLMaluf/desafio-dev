69f8e06c076acf4bb2b7a131760b310b09d7f195
George L. 'Maverick' Maluf <georgelmaluf286@gmail.com>
2021-12-28 13:17:50 -0300

    Fix importer file controller

--- api/app/controllers/api/v1/import_controller.rb
+++ api/app/controllers/api/v1/import_controller.rb
@@ -4,15 +4,16 @@ module Api
   module V1
     class ImportController < Api::BaseApiController
       skip_before_action :verify_authenticity_token
-      before_action :valid_params
-      def import_file
 
+      before_action :valid_params
+
+      def import_file
         path = params['files'].path
 
         return default_error unless path.present?
 
-        if ::Importers::CnabImporter.import path
-          render json: 'Upload and data imported successfully', status: :ok
+        if ::Importers::CnabImporter.import? path
+          render json: 'Upload and data imported successfully.', status: :ok
         else
           default_error
         end
